3064b375ce59b851c63e0ed235fa81a05ea63ab8
Marcin Wojciechowski <mawo78@gmail.com>
2023-12-11 16:28:34 +0100

    optimized searching for empty rows/cols, 2.7s -> 0.1s

--- src/day11_2023.rs
+++ src/day11_2023.rs
@@ -1,6 +1,8 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::HashSet};
+use std::{fs::File, io::{BufReader, BufRead}, collections::HashSet, time::Instant};
 
 pub fn day_11() {
+    let start = Instant::now();
+
     let file = File::open("input/input11_2023.txt").unwrap();
     let lines:Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
 
@@ -34,15 +36,16 @@ pub fn day_11() {
         for j in i+1..gals.len() {
             let g2 = gals[j];
             let d = (g2.0.abs_diff(g1.0) + g2.1.abs_diff(g1.1)) as u64;
-            let pomx:HashSet<usize> = HashSet::from_iter(g1.0.min(g2.0)..g1.0.max(g2.0));
-            let dx = (pomx.intersection(&rows).count()) as u64;
-            let pomy:HashSet<usize> = HashSet::from_iter(g1.1.min(g2.1)..g1.1.max(g2.1));
-            let dy = (pomy.intersection(&cols).count()) as u64;
+            let dx = (rows.iter().filter(|x| g1.0.min(g2.0) < **x && **x < g1.0.max(g2.0)).count()) as u64;
+            let dy = (cols.iter().filter(|x| g1.1.min(g2.1) < **x && **x < g1.1.max(g2.1)).count()) as u64;
             res += d + dx + dy;
             res2 += d + (dx + dy)  * 999999;
             //println!("{:?} - {:?}  d = {}", g1, g2, d);
         }
     }
+    let duration = start.elapsed();
+
+    println!("Time elapsed in calculation is: {:?}", duration);
 
     println!("{}", res);
     println!("{}", res2);
