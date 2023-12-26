aa5287b0f94f87dd56f61e147c7d31e4254db5dc
Marcin Wojciechowski <mawo78@gmail.com>
2023-12-26 01:29:54 +0100

    day 25 brute force attempt 1
    Signed-off-by: Marcin Wojciechowski <mawo78@gmail.com>

--- src/day25_2023.rs
+++ src/day25_2023.rs
@@ -1,7 +1,7 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::{HashSet, HashMap}, borrow::BorrowMut};
+use std::{fs::File, io::{BufReader, BufRead}, collections::{HashSet, HashMap, VecDeque}, borrow::BorrowMut};
 
 pub fn day_25() {
-    let file = File::open("input/input25_2023tst.txt").unwrap();
+    let file = File::open("input/input25_2023.txt").unwrap();
     let lines:Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
 
     let mut graf :Vec<Vec<usize>> = Vec::new();
@@ -29,37 +29,99 @@ pub fn day_25() {
         });
     });
 
-    println!("{:?}", &name2id);
+    //println!("{:?}", &name2id);
 
     lines.iter().for_each(|ln| {
         // jqt: rhn xhk nvd
         let psd:Vec<_> = ln.split(":").collect();
         psd[1].trim().split(" ").for_each(|k|{
             make_edge(&mut graf, psd[0], k, &name2id);
-            make_edge(&mut graf, k, psd[0], &name2id);
+            //make_edge(&mut graf, k, psd[0], &name2id);
         });
     });
 
-    graf.iter().for_each(|k| {println!("{:?} {}", k, k.len())});
-    println!("{:?}", &graf);
-    tarjan(&graf);
+    //graf.iter().for_each(|k| {println!("{:?} {}", k, k.len())});
+    //println!("{:?}", &graf);
+
+    let mut trav:HashMap<(usize, usize), i32> = HashMap::new(); 
+    for v in 0..graf.len(){
+        let mut vis:HashSet<usize> = HashSet::new();
+        bfs(v, &graf, &mut trav, &mut vis);
+    }
+
+    let mut tsort:Vec<(&(usize, usize), &i32)> = trav.iter().collect();
+    tsort.sort_by(|a,b| b.1.cmp(a.1));
+    //println!("{:?}", tsort);
 
     // remove_edge(&mut graf, "hfx", "pzl", &name2id);
-    remove_edge(&mut graf, "pzl", "hfx", &name2id);
-    remove_edge(&mut graf, "bvb", "cmg", &name2id);
-    remove_edge(&mut graf, "cmg", "bvb", &name2id);
-    remove_edge(&mut graf, "nvd", "jqt", &name2id);
-    remove_edge(&mut graf, "jqt", "nvd", &name2id);
+    // remove_edge(&mut graf, "bvb", "cmg", &name2id);
+    // remove_edge(&mut graf, "nvd", "jqt", &name2id);
 
-    graf.iter().for_each(|k| {println!("{:?} {}", k, k.len())});
-    println!("{:?}", &graf);
-    tarjan(&graf);
+    // graf.iter().for_each(|k| {println!("{:?} {}", k, k.len())});
+    // println!("{:?}", &graf);
 
+    for i in 0..300 {
+        println!("{}", i);
+        let edge = tsort[i].0;
+        remove_edge_id(&mut graf, &edge.0, &edge.1);
+        for j in i+1..301 {
+            let edge2 = tsort[j].0;
+            remove_edge_id(&mut graf, &edge2.0, &edge2.1);
+            for k in j+1..302 {
+                let edge3 = tsort[k].0;
+                remove_edge_id(&mut graf, &edge3.0, &edge3.1);
+                
+                let ctd:Vec<i32> = find_ctd(&graf); 
+                if ctd.len() == 2 {
+                    println!("{:?} {}", ctd, ctd[0]*ctd[1]);
+                }
+
+                make_edge_id(&mut graf, &edge3.0, &edge3.1);
+            }            
+            make_edge_id(&mut graf, &edge2.0, &edge2.1);
+        }
+        make_edge_id(&mut graf, &edge.0, &edge.1);
+    }
 
 }
 
+fn find_ctd(graf: &Vec<Vec<usize>>) -> Vec<i32> {
+    let mut vis:HashSet<usize> = HashSet::new();
+    let mut res:Vec<i32> = Vec::new();
+    let mut trav:HashMap<(usize, usize), i32> = HashMap::new(); 
+    for v in 0..graf.len(){
+        if !vis.contains(&v){
+            bfs(v, &graf, &mut trav, &mut vis);
+            if res.is_empty(){
+                res.push(vis.len() as i32);
+            } else {
+                let n = (vis.len() as i32) - res.last().unwrap();
+                res.push(n);
+            }
+        }
+    }
+    res
+}
+
+fn bfs(v: usize, graf: &Vec<Vec<usize>>, trav: &mut HashMap<(usize, usize), i32>, vis:&mut HashSet<usize>)  {
+    let mut qq:VecDeque<usize> = VecDeque::new();
+    qq.push_back(v);
+    while !qq.is_empty() {
+        let u = qq.pop_front().unwrap();
+        vis.insert(u);
+        for w in &graf[u]{
+            if !vis.contains(&w){
+                let k = (u.min(*w), u.max(*w));
+                let val = trav.get(&k).unwrap_or(&0);
+                trav.insert(k, val + 1);
+                qq.push_back(*w);
+            }
+        }
+    }
+}
+
 fn make_edge(graf: &mut Vec<Vec<usize>>, start:&str, cur:&str, name2id:&HashMap<String, usize>) {
-    println!("{:?} -> {:?}", start, cur);
+    //println!("{:?} -> {:?}", start, cur);
     let start_id = name2id.get(start).unwrap();
     let end_id = name2id.get(cur).unwrap();
     make_edge_id(graf, start_id, end_id);
@@ -67,70 +129,20 @@ fn make_edge(graf: &mut Vec<Vec<usize>>, start:&str, cur:&str, name2id:&HashMap<
 
 fn make_edge_id(graf: &mut Vec<Vec<usize>>, start:&usize, cur:&usize) {
     graf[*start].push(*cur);
+    graf[*cur].push(*start);
 }
 
 fn remove_edge(graf: &mut Vec<Vec<usize>>, start:&str, cur:&str, name2id:&HashMap<String, usize>){
-    println!("{:?} -/-> {:?}", start, cur);
+    //println!("{:?} -/-> {:?}", start, cur);
     let start_id = name2id.get(start).unwrap();
     let end_id = name2id.get(cur).unwrap();
     remove_edge_id(graf, start_id, end_id);
 }
 
 fn remove_edge_id(graf: &mut Vec<Vec<usize>>, start:&usize, cur:&usize){
-    println!("{:?} -/-> {:?}", start, cur);
+    //println!("{:?} -/-> {:?}", start, cur);
     let pos = graf[*start].iter().position(|x| x == cur).unwrap();
     graf[*start].remove(pos);
-}
-
-fn dfs(u:usize, graf: &Vec<Vec<usize>>, vis:&mut HashSet<usize>, inn:&mut HashMap<usize, i32>,
-    low:&mut HashMap<usize, i32>, st:&mut Vec<usize>, cnt:&mut i32, sccs:&mut i32){
-    inn.insert(u, *cnt);
-    low.insert(u, *cnt);
-    *cnt += 1;
-    vis.insert(u);
-    st.push(u);
-
-    for v in &graf[u] {
-        if !vis.contains(v) {
-            dfs(*v, graf, vis, inn, low, st, cnt, sccs);
-            let u_val = low.get(&u).unwrap_or(&i32::MAX);
-            let v_val = low.get(&v).unwrap_or(&i32::MAX);
-            low.insert(u, *u_val.min(v_val));
-        } else {
-            let u_val = low.get(&u).unwrap_or(&i32::MAX);
-            let v_in = inn.get(&v).unwrap_or(&i32::MAX);
-            low.insert(u, *u_val.min(v_in));
-        }        
-    }
-    let u_val = low.get(&u).unwrap_or(&i32::MAX);
-    let u_in = inn.get(&u).unwrap_or(&i32::MAX);
-    if u_val == u_in {
-        println!("{} -> {}", u, u);
-        *sccs += 1;
-        //let mut x = 0;
-        loop {
-            let x = st.pop().unwrap();
-            inn.insert(x, i32::MAX);
-            if x == u {
-                break;
-            }
-        } 
-    }
-}
-
-fn tarjan(graf: &Vec<Vec<usize>>){
-    let mut vis:HashSet<usize> = HashSet::new();
-    let mut inn:HashMap<usize, i32> = HashMap::new();
-    let mut low:HashMap<usize, i32> = HashMap::new();
-    let mut st:Vec<usize> = Vec::new();
-    let mut cnt:i32 = 0;
-    let mut sccs:i32 = 0;
-
-    for v in 0..graf.len() {
-        if !vis.contains(&v) {
-            dfs(v, graf, &mut vis, &mut inn, &mut low, &mut st, &mut cnt, &mut sccs);
-        }
-    }
-
-    println!("{} {}", sccs, cnt);
+    let pos = graf[*cur].iter().position(|x| x == start).unwrap();
+    graf[*cur].remove(pos);
 }
